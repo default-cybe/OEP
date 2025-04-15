49a80866de1f7507463f13ba62b85fa31f561778
default-cybe <kaivalyaahir@pnahir.com>
2025-04-15 17:28:34 -0400

    Add Firebase initialization

---
+++ firebase.js
@@ -0,0 +1,5 @@
+// Initialize Firebase using the configuration defined in firebase-config.js.
+// firebase-config.js must be loaded before this script (see the pages that
+// include it). Copy firebase-config.example.js to firebase-config.js first.
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
